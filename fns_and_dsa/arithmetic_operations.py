77529a54012a92388abf2ea75853546efac047b7
Ann <annkarokiadmissions@gmail.com>
2025-07-23 10:31:27 +0300

    Add elif statement

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1, num2, operation):
     if operation == "divide":
         if num2 == 0:
             print("Cannot divide by zero")
-        else:
+        elif num2 != 0:
            return num1 / num2
     else:
         print("Invalid operation. Please enter correct operation")
