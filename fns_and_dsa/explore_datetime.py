0d22f94ecd6808972c368c58860b10ccbe1c0f24
Ann <annkarokiadmissions@gmail.com>
2025-07-26 01:01:55 +0300

    Fix variable name

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -9,8 +9,8 @@ display_current_datetime()
 
 def calculate_future_age():
     future_date = int(input("Enter number of days to add to the current date: "))
-    calculate_future_age = current_date + timedelta(days= future_date)
-    calculate_future_age = calculate_future_age.strftime("%Y-%m-%d")
-    print(f"Future date: {calculate_future_age}")
+    calculate_future_date = current_date + timedelta(days= future_date)
+    calculate_future_date = calculate_future_date.strftime("%Y-%m-%d")
+    print(f"Future date: {calculate_future_date}")
 
 calculate_future_age()
