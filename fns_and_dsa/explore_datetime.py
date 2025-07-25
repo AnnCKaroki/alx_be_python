467460ab4f0859f1338b460c0f3d0f07c9db3049
Ann <annkarokiadmissions@gmail.com>
2025-07-26 00:38:39 +0300

    Add script that performs specified operations on date and time

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,16 @@
+import datetime
+
+current_date = datetime.datetime.now()
+
+def display_current_datetime():
+    return current_date
+
+display_current_datetime()
+
+def calculate_future_age():
+    future_date = int(input("Enter number of days to add to the current date: "))
+    new_date = current_date + datetime.timedelta(days= future_date)
+    new_date = new_date.strftime("%Y-%m-%d")
+    print(new_date)
+
+calculate_future_age()
