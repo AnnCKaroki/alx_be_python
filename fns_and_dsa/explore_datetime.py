5607a1ffd7c8b97cb9cee4c770b1987b7609a4a4
Ann <annkarokiadmissions@gmail.com>
2025-07-26 01:04:49 +0300

    Fix prompt to pass checker

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -8,7 +8,7 @@ def display_current_datetime():
 display_current_datetime()
 
 def calculate_future_age():
-    future_date = int(input("Enter number of days to add to the current date: "))
+    future_date = int(input("Enter the number of days to add to the current date:"))
     calculate_future_date = current_date + timedelta(days= future_date)
     calculate_future_date = calculate_future_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Future date: {calculate_future_date}")
