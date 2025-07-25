7f12327dba121bef7c8113395a2b28ab36c2696b
Ann <annkarokiadmissions@gmail.com>
2025-07-26 01:03:27 +0300

    Formart date and time

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -10,7 +10,7 @@ display_current_datetime()
 def calculate_future_age():
     future_date = int(input("Enter number of days to add to the current date: "))
     calculate_future_date = current_date + timedelta(days= future_date)
-    calculate_future_date = calculate_future_date.strftime("%Y-%m-%d")
+    calculate_future_date = calculate_future_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Future date: {calculate_future_date}")
 
 calculate_future_age()
