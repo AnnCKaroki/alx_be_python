f42bec4dc279e95edd0dcdd68d33d197c05cb2ce
Ann <annkarokiadmissions@gmail.com>
2025-07-31 23:27:02 +0300

    Create bank account class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,21 @@
+class BankAccount:
+    def __init__(self, initial_balance = 0):
+        self._account_balance = initial_balance
+
+
+    def deposit(self, amount):
+        if amount > 0:
+            self._account_balance += amount
+        else:
+            print('Deposit must be greater than 0')
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self._account_balance:
+            self._account_balance -= amount
+            return True
+        else:
+            print('Insufficient funds')
+            return False
+
+    def display_balance(self):
+        print(f"Current balance: {self._account_balance}")
